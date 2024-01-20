ebc0eb650608f43b605e38d485305718958ab9a6
ivanStoynovv <148896796+ivanStoynovv@users.noreply.github.com>
2024-01-20 14:36:12 +0200

    Add about page to homeController

--- src/Controllers/homeController.js
+++ src/Controllers/homeController.js
@@ -4,4 +4,8 @@ router.get('/', (req, res) => {
     res.render("home", {layout: false});
 });
 
+router.get('/about', (req, res) => {
+    res.render("about", {layout: false});
+});
+
 module.exports = router;
--- src/router.js
+++ src/router.js
@@ -1,9 +1,6 @@
 const router = require("express").Router();
 const homeController = require('./Controllers/homeController');
 
-router.get('/about', (req, res) => {
-    res.render("about", {layout: false});
-});
 router.use(homeController);
 
 module.exports = router;
