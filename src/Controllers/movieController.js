546ad28f5bfa02566c61414b0263bf446d882eda
ivanStoynovv <148896796+ivanStoynovv@users.noreply.github.com>
2024-01-20 21:54:33 +0200

    Add body parser

--- src/Controllers/movieController.js
+++ src/Controllers/movieController.js
@@ -1,7 +1,16 @@
 const router = require('express').Router();
+const movieService = require('../services/movieService');
 
 router.get('/create', (req, res) => {
     res.render('create')
-})
+});
+
+router.post('/create', (req, res) => {
+    const movieData = req.body;
+
+    movieService.create(movieData)
+
+    res.send('Movie should be created')
+});
 
 module.exports = router;
