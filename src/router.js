c3dd99cf895aba8c4a3cca619e5818642227bd25
ivanStoynovv <148896796+ivanStoynovv@users.noreply.github.com>
2024-01-20 20:10:48 +0200

    Render create movie page

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const homeController = require('./Controllers/homeController');
+const movieController = require('./Controllers/movieController');
 
 router.use(homeController);
+router.use(movieController);
 
 module.exports = router;
