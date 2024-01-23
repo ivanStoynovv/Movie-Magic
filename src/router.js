e949ec7139c09c3d501f1f9c8ed5fd92529f0089
ivanStoynovv <148896796+ivanStoynovv@users.noreply.github.com>
2024-01-23 22:22:27 +0200

    Add static search page

--- src/router.js
+++ src/router.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 const homeController = require('./Controllers/homeController');
 const movieController = require('./Controllers/movieController');
+const searchController = require('./Controllers/searchController');
 
 router.use(homeController);
 router.use(movieController);
+router.use(searchController);
 
 router.get('*', (req, res) => {
     res.redirect("/404")
