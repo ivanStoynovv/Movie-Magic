fb5f2a412a55568e686bdf087278758d483d4e25
ivanStoynovv <148896796+ivanStoynovv@users.noreply.github.com>
2024-01-28 22:46:32 +0200

    Create movie model

---
+++ src/models/movie.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const {Schema} = mongoose;
+
+const movieSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        require: true
+    },
+    genre: {
+        type: String,
+        require: true
+    },
+    director: {
+        type: String,
+        require: true
+    },
+    year: {
+        type: Number,
+        require: true,
+        min: 1920,
+        max: 2030,
+    },
+    imageUrl: {
+        type: String,
+        require: true,
+        match: /^https?/,
+    },
+    rating: {
+        type: Number,
+        require: true,
+        min: 1,
+        max: 5,
+    },
+    description: {
+        type: String,
+        require: true
+    },
+  });
+
+  const Movie = mongoose.model("Movie", movieSchema);
