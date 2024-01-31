0913d22764e48665b7ff6ca9fa00993f8d163fe8
ivanStoynovv <148896796+ivanStoynovv@users.noreply.github.com>
2024-01-31 21:03:53 +0200

    Get cast info

--- src/Controllers/castController.js
+++ src/Controllers/castController.js
@@ -4,4 +4,11 @@ router.get('/create', (req, res) => {
     res.render('create-cast');
 });
 
+router.post('/create', (req, res) => {
+    const cast = req.body;
+    console.log(cast);
+
+    res.redirect('/');
+});
+
 module.exports = router;
