7d9c934b9e80a6b58f2d69612bfce2d9c8530ac6
ivanStoynovv <148896796+ivanStoynovv@users.noreply.github.com>
2024-02-03 12:29:56 +0200

    Render register page

--- src/Controllers/authController.js
+++ src/Controllers/authController.js
@@ -2,6 +2,10 @@ const router = require("express").Router();
 
 router.get("/login", (req, res) => {
     res.render("login");
+});
+
+router.get("/register", (req, res) => {
+    res.render("register");
 })
 
 module.exports = router;
