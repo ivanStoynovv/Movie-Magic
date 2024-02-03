02925106fb4cdd877a1d18abf17eed384ee94860
ivanStoynovv <148896796+ivanStoynovv@users.noreply.github.com>
2024-02-03 12:27:36 +0200

    Render login page

--- src/router.js
+++ src/router.js
@@ -2,10 +2,12 @@ const router = require("express").Router();
 const homeController = require('./Controllers/homeController');
 const movieController = require('./Controllers/movieController');
 const castController = require("./Controllers/castController");
+const authController = require("./Controllers/authController");
 
 router.use(homeController);
 router.use(movieController);
 router.use("/cast", castController);
+router.use("/auth", authController);
 
 router.get('*', (req, res) => {
     res.redirect("/404")
